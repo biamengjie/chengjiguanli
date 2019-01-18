e2178f2589bcc445deb928b3591c6e3b6be29d91
Your Name <you@example.com>
2019-01-18 10:55:19 +0800

    增加删除学生成绩

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-struct Student{
+struct Student
+{
     int yuwen;
     int shuxue;
     int yingyu;
@@ -22,53 +23,69 @@ int main()
         printf("qingshuruxiangyingdegongnengbianhao:\n");
         int r;
         scanf("%d", &r);
-        if(r==1){
+        if (r == 1)
+        {
             printf("1---qingshuruxueshengxingming:\n");
-            scanf("%s",www[index].name);
+            scanf("%s", www[index].name);
             printf("qingshuruxueshengyuwenchengji:\n");
             int yuwen;
-            scanf("%d",&yuwen);
-            www[index].yuwen=yuwen;
+            scanf("%d", &yuwen);
+            www[index].yuwen = yuwen;
             int shuxue;
-            scanf("%d",&shuxue);
-            www[index].shuxue=shuxue;
+            scanf("%d", &shuxue);
+            www[index].shuxue = shuxue;
             int yingyu;
-            scanf("%d",&yingyu);
-            www[index].yingyu=yingyu;
+            scanf("%d", &yingyu);
+            www[index].yingyu = yingyu;
             index++;
             printf("tianjiachengong,dianjihuichejixu:\n");
             char x;
-            scanf("%c",&x);
-            scanf("%c",&x);
-
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
-        if(r==2){
-
-            
-        }
-        if(r==3){
-            printf("xianshisouyouxueshengchengji:\n");
-            for(int i=0;i<index;i++){
-                printf("%d%s%d%d%d\n",i+1,www[i].name,www[index].yuwen,www[index].shuxue,www[index].yingyu);
+        if (r == 2)
+        {
+            if (index > 0)
+            {
+                index--;
+                printf("shanchuchengong,dianjihuichejixu:\n");
+                char x;
+                scanf("%c", &x);
+                scanf("%c", &x);
             }
-printf("dianjihuichejixu;\n");
-char x;
-            scanf("%c",&x);
-            scanf("%c",&x);
-            
+            else
+            {
+                printf("shanchushibai,dianjihuichejixu:\n");
+                char x;
+                scanf("%c", &x);
+                scanf("%c", &x);
+            }
         }
-        if(r==4){
-            
-        }
-        if(r==5){
-            
-        }
-        if(r==6){
-            
-        }
-        if(r==7){
-            
-        }
+            if (r == 3)
+            {
+                printf("xianshisouyouxueshengchengji:\n");
+                for (int i = 0; i < index; i++)
+                {
+                    printf("%d%s%d%d%d\n", i + 1, www[i].name, www[i].yuwen, www[i].shuxue, www[i].yingyu);
+                }
+                printf("dianjihuichejixu;\n");
+                char x;
+                scanf("%c", &x);
+                scanf("%c", &x);
+            }
+            if (r == 4)
+            {
+            }
+            if (r == 5)
+            {
+            }
+            if (r == 6)
+            {
+            }
+            if (r == 7)
+            {
+            }
     }
-    return 0;
+
+        return 0;
 }
